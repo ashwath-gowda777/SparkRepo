6202da4c4f9aafa007a88bcd040e6d755f166121
ashwath-gowda777 <ashwathgowda216@gmail.com>
2023-12-06 17:30:02 +0530

    This is the file for working on transaction user data

---
+++ src/driver.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+from utils import *
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName("Question1").getOrCreate()
+
+    df1_users = spark.read.csv(r"C:\Users\Admin\OneDrive\Desktop\Diggibyte_Intership\4_Spark\Spark (1)\Spark\spark_1\user.csv", header=True, inferSchema=True)
+    df1_trn = spark.read.csv(r"C:\Users\Admin\OneDrive\Desktop\Diggibyte_Intership\4_Spark\Spark (1)\Spark\spark_1\transaction.csv", header=True, inferSchema=True)
+
+    result_1 = unique_location_count(df1_users, df1_trn,spark)
+    result_2 = products_bought(df1_users, df1_trn,spark)
+    result_3 = total_spending(df1_users, df1_trn, spark)
+
+    result_1.show()
+    result_2.show()
+    result_3.show()
+
+    spark.stop()
---
+++ src/utils.py
@@ -0,0 +1,25 @@
+from pyspark.sql.functions import *
+
+def unique_location_count(df_users, df_transactions, spark):
+    df_users = df_users.select([col(c).alias(c.replace(" ", "")) for c in df_users.columns])
+    df_transactions = df_transactions.select([col(c).alias(c.replace(" ", "")) for c in df_transactions.columns])
+
+    df_join = df_users.join(df_transactions, df_users.user_id == df_transactions.userid, "inner")
+    result_df1 = df_join.select("product_id", "location").distinct().groupBy("product_id", "location").agg(count("location").alias("count_unique_locations"))
+    return result_df1
+
+def products_bought(df_users, df_transactions, spark):
+    df_users = df_users.select([col(c).alias(c.replace(" ", "")) for c in df_users.columns])
+    df_transactions = df_transactions.select([col(c).alias(c.replace(" ", "")) for c in df_transactions.columns])
+
+    df_join = df_users.join(df_transactions, df_users.user_id == df_transactions.userid, "inner")
+    result_df2 = df_join.groupBy("userid").agg(collect_list("product_id").alias("products"))
+    return result_df2
+
+def total_spending(df_users, df_transactions, spark):
+    df_users = df_users.select([col(c).alias(c.replace(" ", "")) for c in df_users.columns])
+    df_transactions = df_transactions.select([col(c).alias(c.replace(" ", "")) for c in df_transactions.columns])
+
+    df_join = df_users.join(df_transactions, df_users.user_id == df_transactions.userid, "inner")
+    result_df3 = df_join.groupBy("user_id", "product_id").agg(sum("price").alias("Total_Spendings"))
+    return result_df3
